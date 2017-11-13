861c9ad3337f0923d8ee23a66e4fccd6267c0252
Shtereva <a.i.shtereva@gmail.com>
2017-11-13 21:30:16 +0200

    Entity Relations

---
+++ Entity Relations/Entity Relations/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+        }
+
+        public StudentSystemContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Resource> Resources { get; set; }
+
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(n => n.Name).HasColumnType("nvarchar(100)");
+
+                entity.Property(p => p.PhoneNumber).IsRequired(false).HasColumnType("char(10)");
+
+                entity.Property(b => b.Birthday).IsRequired(false);
+
+                entity
+                    .HasMany(s => s.HomeworkSubmissions)
+                    .WithOne(h => h.Student)
+                    .HasForeignKey(s => s.StudentId);
+            });
+
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.Property(n => n.Name).HasColumnType("nvarchar(80)");
+
+                entity.Property(d => d.Description).IsRequired(false).HasColumnType("nvarchar(max)");
+
+                entity
+                    .HasMany(c => c.HomeworkSubmissions)
+                    .WithOne(h => h.Course)
+                    .HasForeignKey(c => c.CourseId);
+
+                entity
+                    .HasMany(c => c.Resources)
+                    .WithOne(r => r.Course)
+                    .HasForeignKey(c => c.CourseId);
+            });
+
+            modelBuilder.Entity<Resource>(entity =>
+            {
+                entity.Property(n => n.Name).HasColumnType("nvarchar(50)");
+
+                entity.Property(u => u.Url).HasColumnType("varchar(max)");
+            });
+
+            modelBuilder.Entity<Homework>(entity =>
+            {
+                entity.Property(c => c.Content).HasColumnType("varchar(max)");
+            });
+
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(sc => new
+                {
+                    sc.StudentId,
+                    sc.CourseId
+                });
+        }
+    }
+}
---
+++ Entity Relations/Entity Relations/P03_FootballBetting/Data/FootballBettingContext.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_FootballBetting.Data.Models;
+
+namespace P03_FootballBetting.Data
+{
+    public class FootballBettingContext : DbContext
+    {
+        public FootballBettingContext()
+        {
+        }
+
+        public FootballBettingContext(DbContextOptions options)
+            :base (options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Town> Towns { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<PlayerStatistic> PlayerStatistics { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Bet> Bets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>()
+                .HasMany(c => c.Towns)
+                .WithOne(t => t.Country)
+                .HasForeignKey(c => c.CountryId);
+
+            modelBuilder.Entity<Town>()
+                .HasMany(t => t.Teams)
+                .WithOne(te => te.Town)
+                .HasForeignKey(t => t.TownId);
+
+            modelBuilder.Entity<Position>()
+                .HasMany(p => p.Players)
+                .WithOne(pl => pl.Position)
+                .HasForeignKey(p => p.PositionId);
+
+            modelBuilder.Entity<Color>(entity =>
+            {
+                entity
+                    .HasMany(c => c.PrimaryKitTeams)
+                    .WithOne(t => t.PrimaryKitColor)
+                    .HasForeignKey(c => c.PrimaryKitColorId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity
+                    .HasMany(c => c.SecondaryKitTeams)
+                    .WithOne(t => t.SecondaryKitColor)
+                    .HasForeignKey(c => c.SecondaryKitColorId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<Team>(entity =>
+            {
+                entity
+                    .HasMany(t => t.Players)
+                    .WithOne(p => p.Team)
+                    .HasForeignKey(t => t.TeamId);
+
+                entity
+                    .HasMany(t => t.HomeGames)
+                    .WithOne(g => g.HomeTeam)
+                    .HasForeignKey(t => t.HomeTeamId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity
+                    .HasMany(t => t.AwayGames)
+                    .WithOne(g => g.AwayTeam)
+                    .HasForeignKey(t => t.AwayTeamId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<PlayerStatistic>()
+                .HasKey(pl => new
+                {
+                    pl.PlayerId,
+                    pl.GameId
+                });
+
+            modelBuilder.Entity<Game>()
+                .HasMany(g => g.Bets)
+                .WithOne(b => b.Game)
+                .HasForeignKey(g => g.GameId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Bets)
+                .WithOne(b => b.User)
+                .HasForeignKey(u => u.UserId);
+        }
+    }
+}
